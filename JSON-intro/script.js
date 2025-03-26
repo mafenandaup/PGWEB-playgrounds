20fb3b70d8f8b73c270cde6a3c69946cefdfff67
Maria Fernanda <mafenandaup@gmail.com>
2025-03-26 14:59:57 -0300

    o

--- JSON-intro/script.js
+++ JSON-intro/script.js
@@ -1,6 +1,14 @@
 //Núcleo incial do projeto:
 
-class 
+class Pais{
+    constructor(pai, mae) {
+        this.pai = pai;
+        this.mae = mae;
+        // ao definir atributos sempre use this, porque
+        //senão o JS vai procurar uma variável de escopo maior
+        //fora da classe.
+    }
+} 
 class Aluno{
     constructor(nome, cadastro, nascimento, pais, historico) {
         
