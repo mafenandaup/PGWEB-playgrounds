197760221df8bdb53eacaf720976173fda8036a7
Maria Fernanda <mafenandaup@gmail.com>
2025-03-26 15:32:10 -0300

    ok

--- JSON-intro/script.js
+++ JSON-intro/script.js
@@ -28,6 +28,7 @@ class Aluno{
 function myCode(){
     let pais = new Pais('alberto', 'amanda')
     let historico = ['Cálculo 1', 'POO', 'Arquitetura de computadores']
-    let Aluno = ('Maria', '5592837', '21/06/2003', pais, historico)
+    let Aluno = new Aluno('Maria', '5592837', '21/06/2003', pais, historico)
     // declarando parâmetros/variáveis
+    console.log(Aluno);
 }
